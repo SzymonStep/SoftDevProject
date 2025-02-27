7ea556345a6383792d72b0180bea40a4d8879a96
C00294564 <C00294564@setu.ie>
2025-02-27 10:24:41 +0000

    yu

--- insertTable.java
+++ insertTable.java
@@ -41,8 +41,9 @@ public class insertTable {
         }
     }
 
-    public static void main(String[] args) {
-        // ✅ Example: Insert into Customer Table
+    public static void insertCustomer() 
+    {
+        // Insert into Customer Table
         Map<String, Object> customerData = new HashMap<>();
         customerData.put("firstName", "John");
         customerData.put("lastName", "Doe");
@@ -51,8 +52,11 @@ public class insertTable {
         customerData.put("phoneNumber", "0651234567");
 
         insertRecord("Customer", customerData);
+    }
 
-        // ✅ Example: Insert into Staff Table
+    public static void insertStaff()
+    {
+        // Insert into Staff Table
         Map<String, Object> staffData = new HashMap<>();
         staffData.put("firstName", "Sarah");
         staffData.put("lastName", "Connor");
@@ -61,7 +65,10 @@ public class insertTable {
         staffData.put("phoneNumber", "0851234567");
 
         insertRecord("Staff", staffData);
+    }
 
+    public static void insertEquipmentData()
+    {
         Map<String, Object> equipmentData = new HashMap<>();
         equipmentData.put("equipmentName", "equipmentName1");
         equipmentData.put("equipmentType", "equipmentType");
@@ -70,7 +77,10 @@ public class insertTable {
         equipmentData.put("equipmentPrice", 9.99);
 
         insertRecord("Equipment", equipmentData);
+    }
 
+    public static void insertOrdersData()
+    {
         Map<String, Object> ordersData = new HashMap<>();
         ordersData.put("customerId", "1");
         ordersData.put("orderDate", Date.valueOf(LocalDate.now()));        // Date entry??
@@ -78,7 +88,9 @@ public class insertTable {
         ordersData.put("orderStatus", "Processing");
 
         insertRecord("Orders", ordersData);
-
+    }
+    public static void insertOrderReturnsData()
+    {
         Map<String, Object> OrderReturnsData = new HashMap<>();
         OrderReturnsData.put("orderId", "01");
         OrderReturnsData.put("equipmentId", "1");
@@ -87,13 +99,19 @@ public class insertTable {
         OrderReturnsData.put("replacementRequested", "1"); // Double check logic with foreign keys
 
         insertRecord("OrderReturns", OrderReturnsData);
+    }
 
+    public static void insertOrderAndEquipment()
+    {
         Map<String, Object> orderAndEquipmentData = new HashMap<>();
         orderAndEquipmentData.put("orderId", 2);
         orderAndEquipmentData.put("equipmentId", 1);
 
         insertRecord("OrderAndEquipment", orderAndEquipmentData);
+    }
 
+    public static void insertCustomerFeedbackData()
+    {
         Map<String, Object> customerFeedbackData = new HashMap<>();
         customerFeedbackData.put("customerId", "1");
         customerFeedbackData.put("orderId", "1");
@@ -101,21 +119,30 @@ public class insertTable {
         customerFeedbackData.put("rating", 5);
 
         insertRecord("CustomerFeedback", customerFeedbackData);
+    }
 
+    public static void insertDeliveryData()
+    {
         Map<String, Object> deliveryData = new HashMap<>();
         deliveryData.put("orderId", "1");
         deliveryData.put("estimatedDeliveryTime", Date.valueOf(LocalDate.now())); 
         deliveryData.put("trackingStatus", "Processing");
 
         insertRecord("Delivery", deliveryData);
+    }
 
+    public static void insertReportData()
+    {
         Map<String, Object> reportData = new HashMap<>();
         reportData.put("reportType", "Report1");
         reportData.put("reportData", "1");
         reportData.put("generateDate", Date.valueOf(LocalDate.now()));
 
         insertRecord("Reports", reportData);
+    }
 
+    public static void insertFaultyItemData()
+    {
         Map<String, Object> faultyItemData = new HashMap<>();
         faultyItemData.put("equipmentID", "1");
         faultyItemData.put("batchNumber", "1"); 
@@ -123,5 +150,19 @@ public class insertTable {
         faultyItemData.put("reportedDate", Date.valueOf(LocalDate.now()));
 
         insertRecord("FaultyItems", faultyItemData);
+    }
+    public static void main(String[] args) 
+    {
+        // Call each method to insert records into respective tables
+        insertCustomer();
+        insertStaff();
+        insertEquipmentData();
+        insertOrdersData();
+        insertOrderReturnsData();
+        insertOrderAndEquipment();
+        insertCustomerFeedbackData();
+        insertDeliveryData();
+        insertReportData();
+        insertFaultyItemData();
     }
 }
